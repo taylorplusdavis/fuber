5814c543b7e539e333da85429b0b75aab2ab60df
Taylor Davis <taylor.p.d94@hotmail.com>
2022-12-02 21:20:48 -0600

    deploy

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("./config/db");
 const cors = require("cors");
 
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3002;
 app.use(cors());
