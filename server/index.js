e828d90ee27222cdd36a7f89156358767ddd4578
Taylor Davis <taylor.p.d94@hotmail.com>
2022-12-02 21:22:47 -0600

    deploy

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(
     express.static(path.join(__dirname, "..", ".next", "server", "pages"))
   );
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(
       path.resolve(__dirname, "..", ".next", "server", "pages", "index.html")
     );
