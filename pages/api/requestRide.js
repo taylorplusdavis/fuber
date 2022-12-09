6b627eb1d860517afb95805661755828641238f4
Taylor Davis <taylor.p.d94@hotmail.com>
2022-12-09 15:38:03 -0600

    debug

--- pages/api/requestRide.js
+++ pages/api/requestRide.js
@@ -7,18 +7,17 @@ export default async function handler(req, res) {
     const querySql =
       "SELECT * FROM ride WHERE rider_id = ? AND completed = 'false'";
     const values = [id];
-    const data = await query({ query: querySql, values: values }).then(
-      async (result) => {
-        if (result.length === 0) {
-          const querySql1 =
-            "INSERT INTO ride(rider_id, completed) VALUES(?, 'false')";
-          const values1 = [id];
-          await query({ query: querySql1, values: values1 }).then(() => {
-            res.status(200).json("Ride requested!");
-          });
-        }
+    await query({ query: querySql, values: values }).then(async (result) => {
+      if (result.length === 0) {
+        console.log("second query");
+        const querySql1 =
+          "INSERT INTO ride(rider_id, completed) VALUES(?, 'false')";
+        const values1 = [id];
+        await query({ query: querySql1, values: values1 }).then(() => {
+          res.status(200).json("Ride requested!");
+        });
       }
-    );
+    });
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
